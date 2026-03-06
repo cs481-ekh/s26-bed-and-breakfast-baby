96a3e3abf93384fc61079fbef9fe351783f3e58a
Bloop1542 <alecconn82@gmail.com>
2026-03-05 17:35:05 -0700

    Add signup endpoint to API URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HealthView, UserViewSet
+from .views import HealthView, UserViewSet, SignUpView  # add SignUpView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path("health/", HealthView.as_view()),
+    path("signup/", SignUpView.as_view()),  # add this line
     path("", include(router.urls)),
 ]
