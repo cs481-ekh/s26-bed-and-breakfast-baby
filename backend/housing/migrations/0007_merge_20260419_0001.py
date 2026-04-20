ddd7a0d7e26ef17e6a0260e41af31129503a9589
Alexander Daniluc <alexedaniluc@gmail.com>
2026-04-19 19:06:16 -0600

    Signup page

---
+++ backend/housing/migrations/0007_merge_20260419_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("housing", "0004_alter_bed_id_alter_district_id_alter_facility_id_and_more"),
+        ("housing", "0006_rename_tier_to_track"),
+    ]
+
+    operations = []
